2388c10950874d48f15a70dbc09c1738a47d56dd
angelmartinez4 <angelmartinez200210@gmail.com>
2025-04-02 12:45:25 +0200

    Anadido svg de valoracion media

--- js/graficos.js
+++ js/graficos.js
@@ -49,3 +49,21 @@ function generarBarchartValoraciones(valoraciones) {
     `;
     return svg;
 }
+
+
+function generarValoracionMedia(valoracion) {
+    const MAX_DECIMALES = 2
+    const valRedondeado = Math.round(valoracion * 10**MAX_DECIMALES) / 10**MAX_DECIMALES
+    const valStr = valRedondeado.toString();
+    const decimales = valStr.includes(".") ? valStr.split(".")[1].length : 0; // Lo siento
+    const despXDecimales = (MAX_DECIMALES-decimales)*10 // desplaza estrella izq si no hay decimales
+    const svg = `
+    <svg width="150" height="50" viewBox="0 0 ${125-despXDecimales} 50">
+    <text x="10" y="35" font-size="20" fill="white">${valRedondeado}/10</text>
+    <polygon points="${100-despXDecimales},0 ${106-despXDecimales},20 ${125-despXDecimales},20 ${109-despXDecimales},30 ${115-despXDecimales},50
+    ${100-despXDecimales},38 ${85-despXDecimales},50 ${91-despXDecimales},30 ${75-despXDecimales},20 ${94-despXDecimales},20" 
+        fill="gold" stroke="black" stroke-width="2"/>
+    </svg>
+    `
+    return svg
+}
