d1e7bfccf55321ea0d71ef10047f47878c279166
Krishna Jorda <krishnajordajimenez@gmail.com>
2025-04-02 17:28:34 +0200

    Informacion, api imdb, cambiado imagenes grandes a webp + links a cada pelicula en el carousel

--- js/movie.js
+++ js/movie.js
@@ -19,6 +19,24 @@ function renderData(data) {
 
     container = document.getElementById('grafico');
     container.innerHTML = generarBarchartValoraciones([30, 20, 5, 10, 40, 20, 50, 80, 100, 20]);
+
+    // Informacion
+    container = document.getElementById('director');
+    container.innerHTML = data.director.name;
+
+    container = document.getElementById('genero');
+    container.innerHTML = data.genre.join(', ');
+
+    console.log(data.participant);
+
+    for (var field of ['name', 'birthDate', 'birthPlace', 'jobTitle']) {
+        container = [...document.getElementsByClassName(field)];
+
+        if (data.participant == null) // Si no hay participante mallorquin no mostramos esos campos
+            container.forEach( element => {element.classList.add('d-none');} );
+        else
+            container[1].innerHTML = data.participant[0][field];
+    }
 }
 
 async function loadMovie() {
@@ -31,6 +49,7 @@ async function loadMovie() {
         movies = movies['@graph'];
 
         renderData(movies[movieId]);
+        loadIMDBRating(movies[movieId].sameAs);
     } catch (error) {
         console.error('Error loading movie:', error);
     }
@@ -39,8 +58,14 @@ async function loadMovie() {
 loadMovie();
 
 
-async function loadIMDBRating() {
+async function loadIMDBRating(url) {
     var rating;
+    var runtime;
+    var year;
+    console.log(url);
+    const regex = /\/title\/(tt\d+)/;
+    const imdbcode = regex.exec(url)[1];
+
     try {
       const response = await fetch('https://graph.imdbapi.dev/v1', {
         method: 'POST',
@@ -49,11 +74,13 @@ async function loadIMDBRating() {
         },
         body: JSON.stringify({
           query: `{
-            title(id: "tt0468569") {
-              rating {
-                aggregate_rating
-                votes_count
-              }
+            title(id: "${imdbcode}") {
+                rating {
+                    aggregate_rating
+                    votes_count
+                }
+                start_year
+                runtime_minutes
             }
           }`
         })
@@ -61,11 +88,18 @@ async function loadIMDBRating() {
   
       const data = await response.json();
       rating = [data.data.title.rating.aggregate_rating, data.data.title.rating.votes_count];
+      runtime = data.data.title.runtime_minutes;
+      year = data.data.title.start_year;
     } catch (error) {
       console.error('Error fetching IMDB data:', error);
     }
 
+    var str = "";
+    if (year != null)
+        str += year;
+    if (runtime != null)
+        str += " / "+runtime+" mins";
 
+    document.getElementById('year').innerHTML = str;
+    document.getElementById('imdbrating').innerHTML = rating[0].toLocaleString('es-ES');;
   }
-
-  loadIMDBRating();
