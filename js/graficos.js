9339dbd71a777a33f04fb4fbdd50353091a8ad88
angelmartinez4 <angelmartinez200210@gmail.com>
2025-04-03 03:24:13 +0200

    Anadido mapa con usuarios del JSON

--- js/graficos.js
+++ js/graficos.js
@@ -50,9 +50,13 @@ function generarBarchartValoraciones(valoraciones) {
     return svg;
 }
 
-
+/**
+ * Genera svg dinamico adaptando el tamano al numero de decimales de la valoracion media
+ * @param {number} valoracion numero con o sin decimales 
+ * @returns 
+ */
 function generarValoracionMedia(valoracion) {
-    const MAX_DECIMALES = 5
+    const MAX_DECIMALES = 2
     const valRedondeado = Math.round(valoracion * 10**MAX_DECIMALES) / 10**MAX_DECIMALES
     const valStr = valRedondeado.toString();
     const decimales = valStr.includes(".") ? valStr.split(".")[1].length : 0; // Lo siento
@@ -68,3 +72,51 @@ function generarValoracionMedia(valoracion) {
     `
     return svg
 }
+
+// Función para obtener las coordenadas de los usuarios y devolverlas
+function obtenerCoordenadasUsuarios() {
+    return fetch("js/users.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error al obtener el archivo JSON");
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data["@graph"]
+          .filter(user => user.homeLocation && user.homeLocation.geo)
+          .map(user => ({
+            name: user.name,
+            latitude: parseFloat(user.homeLocation.geo.latitude),
+            longitude: parseFloat(user.homeLocation.geo.longitude)
+          }));
+      })
+      .catch(error => {
+        console.error("Error en la obtención de coordenadas:", error);
+        return [];
+      });
+  }  
+
+function dibujarMapa(coordenadas, contenedorId) {
+    const mapa = L.map(contenedorId);
+    // base del mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+    const puntos = [];
+    coordenadas.forEach(coord => {
+           L.marker([coord.latitude, coord.longitude])
+            .addTo(mapa)
+            .bindPopup(`<b>${coord.name}</b><br>Lat: ${coord.latitude}<br>Lon: ${coord.longitude}`);        
+        puntos.push([coord.latitude, coord.longitude]);
+    });
+
+    // ajustar vista para que se vean todos los puntos
+    if (puntos.length > 0) {
+        const bounds = L.latLngBounds(puntos);
+        mapa.fitBounds(bounds);
+    }
+    return mapa;
+}
+
+  
