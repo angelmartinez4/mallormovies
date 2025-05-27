5ec576af788230a30dc3704d3ec491a725afd59b
angelmartinez4 <angelmartinez200210@gmail.com>
2025-05-27 15:58:16 +0200

    Marcadores verdes para JSON de otras pelis

--- js/mapa.js
+++ js/mapa.js
@@ -63,8 +63,17 @@ function dibujarMapa(userCoords, movieCoords, contenedorId) {
       puntos.push([coord.latitude, coord.longitude]);
   });
 
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
   movieCoords.forEach(coord => {
-      L.marker([coord.latitude, coord.longitude])
+      L.marker([coord.latitude, coord.longitude], {icon: greenIcon})
           .addTo(mapa)
           .bindPopup(`<a href="${coord.url}"><b>${coord.locationName}</b></a><br><b>Pelicula:</b> ${coord.name}<br><b>Rol del sitio:</b> ${coord.desc}`);        
       puntos.push([coord.latitude, coord.longitude]);
